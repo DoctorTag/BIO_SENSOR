7ca1fbdbd75c07110b753f19766b83dc1179e593
seanfan <seanfan@qq.com>
2018-09-21 10:09:36 +0800

    Delete cvi software

--- Demo_cvi_2017/README.md
+++
@@ -1,2 +0,0 @@
-# Demo_labwindows_cvi
-Local demo software of FeelKit  about labwindows cvi 2017
--- Demo_cvi_2017/cvibuild.bio_cvi/Debug/BuildStep.bat
+++
@@ -1,5 +0,0 @@
-@call %1.bat > %1.out
-@if %errorlevel% neq 0 (
-  @echo Build Step failed for %1
-  @exit %errorlevel%)
-@echo Build Step complete for %1
